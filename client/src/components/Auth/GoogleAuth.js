ee6376f6191f2c9bcac6e393afbb063c2a0eebc3
milktegi <milkteegi@gmail.com>
2019-01-03 11:19:19 +0900

    get auth state from gapi & added user's state on header

--- client/src/components/Auth/GoogleAuth.js
+++ client/src/components/Auth/GoogleAuth.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = {
+    isSignedIn: null
+  };
 
-	componentDidMount() {
-		window.gapi.load('client:auth2', () => {
-			// clientId와 scope을 초기화 
-			window.gapi.client.init({
-				clientId: '334569413873-au5ulcefah2v47c50a8f6oprv3nhocmi.apps.googleusercontent.com',
-				scope: 'email'
-			})
-		});
-	}
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      // clientId와 scope을 초기화
+      window.gapi.client
+        .init({
+          clientId:
+            '334569413873-au5ulcefah2v47c50a8f6oprv3nhocmi.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>로그인</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>로그아웃</div>;
+    } else {
+      return <div>로그인 아직</div>;
+    }
+  }
 
   render() {
-    return <div>구글 어스</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
