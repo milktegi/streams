b9821f218a5ce9f5c86b0617be6d9657b7b664a1
milktegi <milkteegi@gmail.com>
2019-01-03 12:56:41 +0900

    added types & currentUser's id as a payload

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,19 @@
 // 상태 관리ㅣㅣㅣㅣ
 
-export const signIn = () => {
+import { SIGN_IN, SIGN_OUT} from './types';
+
+// when got a signal of 
+// signIn attempted, 
+// get userId and return payload
+export const signIn = (userId) => {
   return {
-    type: 'SIGN_IN'
+    type: SIGN_IN,
+    payload: userId
   };
 };
 
 export const signOut = () => {
   return {
-    type: 'SIGN_OUT'
+    type: SIGN_OUT
   };
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,15 +1,18 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
 const INITIAL_STATE = {
-	isSignedIn: null
+  isSignedIn: null,
+	userId: null
 };
 
 export default (state = INITIAL_STATE, action) => {
-	// 액션 타입 검사 후 복사? 
-	switch(action.type){
-		case 'SIGN_IN':
-		return {...state, isSignedIn: true };
-		case 'SIGN_OUT':
-		return{...state, isSignedIn: false}
-		default:
-		return state;
-	}
-}
+  // 액션 타입 검사 후 복사?
+  switch (action.type) {
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
+    default:
+      return state;
+  }
+};
