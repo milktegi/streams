07a71a006f649e48fd45c7559e2cd7a9ed6b11cb
milktegi <milkteegi@gmail.com>
2019-01-03 23:59:08 +0900

    created action creator to request formValues

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,13 @@
-// 상태 관리ㅣㅣㅣㅣ
+// 상태 관리
+// axios 인스턴스 수입
+import streams from '../apis/streams';
 
-import { SIGN_IN, SIGN_OUT} from './types';
+import { SIGN_IN, SIGN_OUT } from './types';
 
-// when got a signal of 
-// signIn attempted, 
+// when got a signal of
+// signIn attempted,
 // get userId and return payload
-export const signIn = (userId) => {
+export const signIn = userId => {
   return {
     type: SIGN_IN,
     payload: userId
@@ -16,4 +18,8 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+export const createStream = formValues => async dispatch => {
+  const response = streams.post('/streams', formValues);
 };
