8b0c9b0cd4938cc3e1cfe1b24b994601a072b0cd
milktegi <milkteegi@gmail.com>
2019-01-04 11:13:19 +0900

    Added case of fetching & creating inside streamReducer

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import RouteDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 
+// 리덕스 푹
+import reduxThunk from 'redux-thunk';
 
 // 외부 컴포넌트 import
 import App from './components/App';
@@ -12,11 +14,13 @@ import reducers from './reducers';
 // redux setup!
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducers, composeEnhancers(applyMiddleware())
-  );
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 RouteDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, document.querySelector('#root')
+  </Provider>,
+  document.querySelector('#root')
 );
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,8 @@
-import { SIGN_IN, SIGN_OUT } from '../actions/types';
+import { SIGN_IN, SIGN_OUT, EDIT_STREAM } from '../actions/types';
 
 const INITIAL_STATE = {
   isSignedIn: null,
-	userId: null
+  userId: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -16,3 +16,33 @@ export default (state = INITIAL_STATE, action) => {
       return state;
   }
 };
+
+// // sample code
+// // state 타입이 배열인 경우
+
+// const streamReducer = (state = [], action) => {
+//   switch (action.type) {
+//     case EDIT_STREAM:
+//       return state.map(stream => {
+//         if (stream.id === action.payload.id) {
+//           return action.payload;
+//         } else {
+//           return stream;
+//         }
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+// const streamReducer = (state = {}, action) => {
+//   switch(action.type) {
+//     case EDIT_STREAM:
+//     // const newState = {... state};
+//     // newState[action.payload.id] = action.payload;
+//     // return newState;
+//     return {...state, [action.payload.id] : action.payload};
+//     default:
+//     return state; 
+//   }
+// }
