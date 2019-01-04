bc61c5754cfa9b53e0cccfca991a1173e003e88c
milktegi <milkteegi@gmail.com>
2019-01-04 12:50:56 +0900

    rendered streamList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,18 +1,55 @@
-// 일단은 함수형으로 
+// 일단은 함수형으로
 
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
-	
-
-	return(
-		<div>
-			나는 streamlist이당ㅎㅎㅎ 
-		</div>
-	)
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="row" key={stream.id}>
+          <div className="col s12">
+            <div className="card">
+              <div className="card-content">
+                <div className="row">
+                  <div className="col s12 m6">
+										 <i className="material-icons prefix">camera_roll</i>
+                    <span className="card-title">스트리밍 리스트</span>
+                    <div className="col s12 m6" key={stream.id}>
+              
+                        <h5>title: {stream.title}</h5>
+                        <h6>description: {stream.description}</h6> 
+                
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div>{this.renderList()}</div>
+      </div>
+    );
+  }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
