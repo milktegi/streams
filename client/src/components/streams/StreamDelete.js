b7323b47a5e590165ea7c9998a063c227590ab70
milktegi <milkteegi@gmail.com>
2019-01-06 12:27:40 +0900

    implemented delete a stream func

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
 
@@ -13,21 +14,34 @@ class StreamDelete extends React.Component {
 	 this.props.fetchStream(this.props.match.params.id)
  }
   renderActions() {
-   return (
+		// const id = this.props.match.params.id;
+
+		const { id } = this.props.match.params;
+    return (
       <React.Fragment>
-        <button className="ui button negative">삭제</button>
-        <button className="ui button">취소</button>
+        <button
+				onClick={() => this.props.deleteStream(id) } 
+				className="ui button negative">삭제	</button>
+        <Link to="/" className="ui button">취소</Link>
       </React.Fragment>
     );
   }
 
+
+	renderContent() {
+		if(!this.props.stream){
+			return 'ㄹㅇ?'
+		}
+		return `are you sure you want to delete 
+		the stream with title:${'  '}${this.props.stream.title}`
+	}
   render() {
+	
     return (
       <div>
-        Stream Delete
         <Modal
           title="Delete Stream"
-          content="정말로 삭제하시겠습니까?"
+          content={this.renderContent()}
           actions={this.renderActions()}
           onDismiss={() => history.push('/')}
         />
@@ -36,4 +50,14 @@ class StreamDelete extends React.Component {
   }
 }
 
-export default connect(null, { fetchStream })(StreamDelete);
+const mapStateToProps = (state, ownProps) => {
+
+	return {
+		stream: state.streams[ownProps.match.params.id]
+	}
+};
+
+
+export default connect(mapStateToProps, { 
+	fetchStream, deleteStream
+ })(StreamDelete);
